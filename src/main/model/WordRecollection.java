c3dec1936bf13606ed73980219435a9d2963e8f1
goete <ebmg55@icloud.com>
2024-02-06 15:32:52 -0800

    Bare bones

---
+++ src/main/model/WordRecollection.java
@@ -0,0 +1,9 @@
+package model;
+
+public class WordRecollection extends Game {
+
+    // REQUIRES: difficulty is one of Easy, Medium, Hard
+    public WordRecollection(String difficulty) {
+        super(difficulty);
+    }
+}
