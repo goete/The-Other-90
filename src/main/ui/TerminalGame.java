baab751b8468a39d8ebfeb24f34a193152acea7c
goete <ebmg55@icloud.com>
2024-02-15 19:22:06 -0800

    Finished the high scored functions

--- src/main/ui/TerminalGame.java
+++ src/main/ui/TerminalGame.java
@@ -19,9 +19,12 @@ public class TerminalGame {
     private SumElimination sumEliminationGame;
     private Leaderboards leaderboard;
     private ArrayList<Integer> collectionOfNumbers;
+    private ArrayList<Player> topN;
     private boolean highScoreViewing;
     private Player player;
     private String playerAnswer;
+    private int numOfPlayersToView;
+    private int position;
 
 
     public TerminalGame() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException,
@@ -31,6 +34,7 @@ public class TerminalGame {
         leaderboard = new Leaderboards();
         collectionOfNumbers = new ArrayList<>();
         highScoreViewing = false;
+        position = 1;
 
         // EVERYTHING ABOVE HERE
         gameMechanics();
@@ -50,12 +54,13 @@ public class TerminalGame {
                 gameMode = "Word Recollection ";
             } else if (nextInput.equals("HS")) {
                 highScoreViewing = true;
-                highScoreViewing();
             } else {
                 gameMode = "Sum Elimination ";
             }
             if (!highScoreViewing) {
                 difficultySelectingAndContinuing();
+            } else {
+                highScoreViewing();
             }
         }
     }
@@ -72,13 +77,152 @@ public class TerminalGame {
             gameDifficulty = "Hard";
         }
         handleGameStarting();
-        System.out.println("Say YES to quit game, anything else to play a new game");
+        System.out.println("Say YES to close program, anything else to play a new game");
         if (scanner.next().equals("YES")) {
             gameContinues = false;
         } // Otherwise loop continues
     }
 
-    private void highScoreViewing() {
+    private void highScoreViewing() throws InvocationTargetException, IllegalAccessException {
+        while (highScoreViewing) {
+            System.out.println("What game would you like to view?");
+            System.out.println("Type Word to view Word Recollection");
+            System.out.println("Type Sum to view Sum Elimination");
+            nextInput = scanner.next();
+            if (nextInput.equals("Word")) {
+                gameMode = "Word Recollection ";
+            } else if (nextInput.equals("HS")) {
+                highScoreViewing = true;
+            } else {
+                gameMode = "Sum Elimination ";
+            }
+            highScoreDifficultyHandling();
+        }
+    }
+
+    private void highScoreDifficultyHandling() throws InvocationTargetException, IllegalAccessException {
+        System.out.println("Type Easy, Medium or Hard to view that difficulty");
+        nextInput = scanner.next();
+        if (nextInput.equals("Easy")) {
+            gameDifficulty = "Easy";
+        } else if (nextInput.equals("Medium")) {
+            gameDifficulty = "Medium";
+        } else {
+            gameDifficulty = "Hard";
+        }
+        System.out.println("How many of the top players would you like to view?");
+        numOfPlayersToView = scanner.nextInt();
+        if (gameMode.contains("Word")) {
+            handleHighScorePrintingOutWord();
+        } else {
+            handleHighScorePrintingOutSum();
+        }
+        System.out.println("Would you like to view high scores again?"
+                + " If so, type YES, to return to main menu, say anything else");
+        nextInput = scanner.next();
+        if (!nextInput.equals("YES")) {
+            this.highScoreViewing = false;
+        }
+    }
+
+    private void handleHighScorePrintingOutSum() throws InvocationTargetException, IllegalAccessException {
+        position = 1;
+        if (gameDifficulty.equals("Easy")) {
+            if (leaderboard.getSumEliminationEasy().isEmpty()) {
+                System.out.println("Nobody has played that game yet! Go be the first!");
+            } else {
+                handleSumEasyHighScore();
+            }
+        } else if (gameDifficulty.equals("Medium")) {
+            if (leaderboard.getSumEliminationMedium().isEmpty()) {
+                System.out.println("Nobody has played that game yet! Go be the first!");
+            } else {
+                handleSumMediumHighScore();
+            }
+        } else {
+            if (leaderboard.getSumEliminationHard().isEmpty()) {
+                System.out.println("Nobody has played that game yet! Go be the first!");
+            } else {
+                handleSumHardHighScore();
+            }
+        }
+    }
+
+    private void handleSumHardHighScore() throws InvocationTargetException, IllegalAccessException {
+        topN = leaderboard.getSumEliminationHard().getTopNPlayers(numOfPlayersToView);
+        for (Player player : topN) {
+            System.out.println(position + ". " + player.getName() + " has a score of "
+                    + player.getSumEliminationHardHighScore());
+            position++;
+        }
+    }
+
+    private void handleSumMediumHighScore() throws InvocationTargetException, IllegalAccessException {
+        topN = leaderboard.getSumEliminationMedium().getTopNPlayers(numOfPlayersToView);
+        for (Player player : topN) {
+            System.out.println(position + ". " + player.getName() + " has a score of "
+                    + player.getSumEliminationMediumHighScore());
+            position++;
+        }
+    }
+
+    private void handleSumEasyHighScore() throws InvocationTargetException, IllegalAccessException {
+        topN = leaderboard.getSumEliminationEasy().getTopNPlayers(numOfPlayersToView);
+        for (Player player : topN) {
+            System.out.println(position + ". " + player.getName() + " has a score of "
+                    + player.getSumEliminationEasyHighScore());
+            position++;
+        }
+    }
+
+    private void handleHighScorePrintingOutWord() throws InvocationTargetException, IllegalAccessException {
+        position = 1;
+        if (gameDifficulty.equals("Easy")) {
+            if (leaderboard.getWordRecollectionEasy().isEmpty()) {
+                System.out.println("Nobody has played that game yet! Go be the first!");
+            } else {
+                handleWordEasyHighScore();
+            }
+        } else if (gameDifficulty.equals("Medium")) {
+            if (leaderboard.getWordRecollectionMedium().isEmpty()) {
+                System.out.println("Nobody has played that game yet! Go be the first!");
+            } else {
+                handleWordMediumHighScore();
+            }
+        } else {
+            if (leaderboard.getWordRecollectionHard().isEmpty()) {
+                System.out.println("Nobody has played that game yet! Go be the first!");
+            } else {
+                handleWordHardHighScore();
+            }
+        }
+    }
+
+    private void handleWordHardHighScore() throws InvocationTargetException, IllegalAccessException {
+        topN = leaderboard.getWordRecollectionHard().getTopNPlayers(numOfPlayersToView);
+        for (Player player : topN) {
+            System.out.println(position + ". " + player.getName() + " has a score of "
+                    + player.getWordRecollectionHardHighScore());
+            position++;
+        }
+    }
+
+    private void handleWordMediumHighScore() throws InvocationTargetException, IllegalAccessException {
+        topN = leaderboard.getWordRecollectionMedium().getTopNPlayers(numOfPlayersToView);
+        for (Player player : topN) {
+            System.out.println(position + ". " + player.getName() + " has a score of "
+                    + player.getWordRecollectionMediumHighScore());
+            position++;
+        }
+    }
+
+    private void handleWordEasyHighScore() throws InvocationTargetException, IllegalAccessException {
+        topN = leaderboard.getWordRecollectionEasy().getTopNPlayers(numOfPlayersToView);
+        for (Player player : topN) {
+            System.out.println(position + ". " + player.getName() + " has a score of "
+                    + player.getWordRecollectionEasyHighScore());
+            position++;
+        }
     }
 
     private void handleGameStarting() throws InvocationTargetException, IllegalAccessException, FileNotFoundException {
@@ -125,7 +269,7 @@ public class TerminalGame {
             sumEliminationGame.addOneToScore();
         } else {
             leaderboard.addPlayerToLeaderboard(player, sumEliminationGame);
-            System.out.println("Here");
+            System.out.println("Game Over. You scored " + sumEliminationGame.getCurrentScore());
             gameInProgress = false;
         }
     }
@@ -159,6 +303,7 @@ public class TerminalGame {
             wordRecollectionGame.addOneToScore();
         } else {
             leaderboard.addPlayerToLeaderboard(player, wordRecollectionGame);
+            System.out.println("Game Over. You scored " + wordRecollectionGame.getCurrentScore());
             System.out.println("These are all the words you saw:");
             System.out.println(wordRecollectionGame.getWordsFound().toString());
             gameInProgress = false;
