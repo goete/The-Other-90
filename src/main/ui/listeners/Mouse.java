f1041913a93b36539dd076facabb18bd982bd88b
goete <ebmg55@icloud.com>
2024-03-24 03:32:27 -0700

    Adding mouse listener

---
+++ src/main/ui/listeners/Mouse.java
@@ -0,0 +1,33 @@
+package ui.listeners;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+
+// Citation: https://github.com/goete/Scrabble
+public class Mouse extends MouseAdapter {
+
+    // if a mouse button has been pressed down
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    // if a mouse button has been released
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    // if the scroll wheel has been moved
+    @Override
+    public void mouseWheelMoved(MouseWheelEvent e) {
+
+    }
+
+    // if the mouse has moved positions
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+}
