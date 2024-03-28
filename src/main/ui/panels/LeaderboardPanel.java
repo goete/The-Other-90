7f4d4e75faa74e05d63bfef833837be498e04298
goete <ebmg55@icloud.com>
2024-03-27 17:55:52 -0700

    Fully functioning leaderboards

--- src/main/ui/panels/LeaderboardPanel.java
+++ src/main/ui/panels/LeaderboardPanel.java
@@ -1,16 +1,350 @@
 package ui.panels;
 
-import javax.swing.*;
-import java.awt.*;
+import model.Leaderboards;
+import model.Player;
+import ui.GameGUI;
 
-public class LeaderboardPanel extends JPanel {
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class LeaderboardPanel extends JPanel implements ActionListener {
 
     private final int height;
     private final int width;
+    private Leaderboards leaderboards;
+    private boolean gettingInformation;
+    private ButtonGroup buttonGameGroup;
+    private ButtonGroup buttonDifficultyGroup;
+    private JRadioButton wordRecollectionButton;
+    private JRadioButton sumEliminationButton;
+    private JRadioButton easyButton;
+    private JRadioButton mediumButton;
+    private JRadioButton hardButton;
+    private JButton backButton;
+    private JTextField numberOfDesiredPlayersTextField;
+    private JButton goAndShowButton;
+    private JButton continueButton;
+    private final GameGUI gameGUI;
+    private int numOfPlayersToView;
+    private int position;
+    private ArrayList<Player> topN;
+    private String leaderboardText;
+    private String errorText;
 
-    public LeaderboardPanel(int width, int height) {
+    public LeaderboardPanel(int width, int height, Leaderboards leaderboards, GameGUI gameGUI) {
         this.setPreferredSize(new Dimension(width, height));
         this.height = height;
         this.width = width;
+        this.leaderboards = leaderboards;
+        this.gettingInformation = true;
+        this.setLayout(null);
+        this.setUpButtons();
+        this.gameGUI = gameGUI;
+        this.numOfPlayersToView = 0;
+        this.leaderboardText = "";
+        this.errorText = "";
     }
+
+    private void setUpButtons() {
+        this.setUpGameButtons();
+        this.setUpDifficultyButtons();
+        this.setUpOtherScreenOneButtons();
+
+        this.setUpContinueButton();
+    }
+
+    private void setUpContinueButton() {
+        this.continueButton = new JButton("Continue");
+        this.continueButton.setSize(200, 50);
+        this.continueButton.setLocation(200, 700);
+        this.continueButton.addActionListener(this);
+    }
+
+    private void setUpOtherScreenOneButtons() {
+        this.backButton = new JButton("⮪");
+        this.backButton.addActionListener(this);
+        this.backButton.setSize(50, 50);
+        this.backButton.setLocation(0, 0);
+
+        this.numberOfDesiredPlayersTextField = new JTextField();
+        this.numberOfDesiredPlayersTextField.setLocation(275, 550);
+        this.numberOfDesiredPlayersTextField.setSize(50, 50);
+        this.numberOfDesiredPlayersTextField.addActionListener(this);
+
+        this.goAndShowButton = new JButton("✓");
+        this.goAndShowButton.addActionListener(this);
+        this.goAndShowButton.setLocation(275, 650);
+        this.goAndShowButton.setSize(50, 50);
+    }
+
+    public void resetLeaderboardAfterLoad(Leaderboards l) {
+        this.leaderboards = l;
+    }
+
+
+    private void setUpGameButtons() {
+        this.buttonGameGroup = new ButtonGroup();
+        this.wordRecollectionButton = new JRadioButton("Word Recollection");
+        this.wordRecollectionButton.addActionListener(this);
+        this.sumEliminationButton = new JRadioButton("Sum Elimination");
+        this.sumEliminationButton.addActionListener(this);
+
+        buttonGameGroup.add(this.sumEliminationButton);
+        buttonGameGroup.add(this.wordRecollectionButton);
+
+        Dimension size = new Dimension(150, 100);
+        this.sumEliminationButton.setSize(size);
+        this.wordRecollectionButton.setSize(size);
+
+        this.sumEliminationButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        this.sumEliminationButton.setVerticalTextPosition(JRadioButton.TOP);
+        this.wordRecollectionButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        this.wordRecollectionButton.setVerticalTextPosition(JRadioButton.TOP);
+
+        this.sumEliminationButton.setLocation(width / 2 - 125, 250);
+        wordRecollectionButton.setLocation(width / 2 + 25, 250);
+    }
+
+    private void setUpDifficultyButtons() {
+        this.buttonDifficultyGroup = new ButtonGroup();
+        this.easyButton = new JRadioButton("Easy");
+        this.easyButton.addActionListener(this);
+        this.mediumButton = new JRadioButton("Medium");
+        this.mediumButton.addActionListener(this);
+        this.hardButton = new JRadioButton("Hard");
+        this.hardButton.addActionListener(this);
+        buttonDifficultyGroup.add(this.easyButton);
+        buttonDifficultyGroup.add(this.mediumButton);
+        buttonDifficultyGroup.add(this.hardButton);
+        Dimension size = new Dimension(60, 50);
+        this.easyButton.setSize(size);
+        this.mediumButton.setSize(size);
+        this.hardButton.setSize(size);
+
+        this.easyButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        this.easyButton.setVerticalTextPosition(JRadioButton.TOP);
+        this.mediumButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        this.mediumButton.setVerticalTextPosition(JRadioButton.TOP);
+        this.hardButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        this.hardButton.setVerticalTextPosition(JRadioButton.TOP);
+
+        this.easyButton.setLocation(this.width / 2 - 30 - 60, 400);
+        this.mediumButton.setLocation(this.width / 2 - 30, 400);
+        this.hardButton.setLocation(this.width / 2 + 30, 400);
+
+    }
+
+    @Override
+    public void paintComponent(final Graphics graphics) {
+        graphics.setColor(Color.LIGHT_GRAY);
+        graphics.clearRect(0, 0, width, height);
+        graphics.fillRect(0, 0, width, height);
+        if (gettingInformation) {
+            paintQuestionButtons(graphics);
+            paintGettingTexts(graphics);
+        } else {
+            paintContinueButton(graphics);
+            paintCorrectLeaderboard(graphics);
+        }
+
+    }
+
+    private void paintCorrectLeaderboard(Graphics graphics) {
+        graphics.setFont(new Font("MonoLisa", Font.PLAIN, 15));
+        graphics.setColor(Color.BLACK);
+        drawStringWithLineBreaks(graphics, this.leaderboardText, 50, 50);
+    }
+
+    private void drawStringWithLineBreaks(Graphics g, String text, int x, int y) {
+        for (String line : text.split("\n")) {
+            g.drawString(line, x, y += g.getFontMetrics().getHeight());
+        }
+    }
+
+    private void paintContinueButton(Graphics graphics) {
+        this.add(this.continueButton);
+
+        this.remove(this.hardButton);
+        this.remove(this.easyButton);
+        this.remove(this.mediumButton);
+        this.remove(this.wordRecollectionButton);
+        this.remove(this.sumEliminationButton);
+        this.remove(this.goAndShowButton);
+        this.remove(numberOfDesiredPlayersTextField);
+    }
+
+    private void paintGettingTexts(Graphics graphics) {
+        graphics.setColor(Color.BLACK);
+        graphics.setFont(new Font("MonoLisa", Font.BOLD, 18));
+        graphics.drawString("Please select the desired mode, difficulty, and player count", 25, 80);
+        graphics.setFont(new Font("MonoLisa", Font.PLAIN, 12));
+        graphics.drawString("Input the number of players you would like to see",
+                this.numberOfDesiredPlayersTextField.getX() - 100,
+                this.numberOfDesiredPlayersTextField.getY() - 20);
+        this.add(this.numberOfDesiredPlayersTextField);
+        this.add(this.goAndShowButton);
+        graphics.setColor(Color.RED);
+        graphics.drawString(errorText, this.numberOfDesiredPlayersTextField.getX() - 50,
+                this.numberOfDesiredPlayersTextField.getY() + 90);
+
+    }
+
+    private void paintQuestionButtons(Graphics graphics) {
+        this.add(easyButton);
+        this.add(mediumButton);
+        this.add(hardButton);
+
+        this.add(wordRecollectionButton);
+        this.add(sumEliminationButton);
+
+        this.add(backButton);
+        this.add(numberOfDesiredPlayersTextField);
+
+        this.remove(this.continueButton);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == this.goAndShowButton) {
+            if (this.buttonGameGroup.getSelection() != null && this.buttonDifficultyGroup != null
+                    && this.numberOfDesiredPlayersTextField.getText() != null
+                    && isAllNumbers(this.numberOfDesiredPlayersTextField.getText())) {
+                switchAndShowDesired();
+                setTheTextForLeaderboards();
+            } else {
+                this.errorText = "Please select valid inputs";
+            }
+        } else if (e.getSource() == this.continueButton) {
+            switchAndShowDesired();
+        } else if (e.getSource() == this.backButton) {
+            this.gameGUI.showFrameByString(gameGUI.getTitleMenu());
+            this.gettingInformation = true;
+            this.errorText = "";
+        }
+        repaint();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the leaderboard text
+    private void setTheTextForLeaderboards() {
+        this.numOfPlayersToView = Integer.parseInt(this.numberOfDesiredPlayersTextField.getText());
+        this.position = 1;
+        try {
+            if (this.wordRecollectionButton.isSelected()) {
+                if (this.easyButton.isSelected()) {
+                    this.leaderboardText = handleWordEasyHighScore();
+                } else if (this.mediumButton.isSelected()) {
+                    this.leaderboardText = handleWordMediumHighScore();
+                } else if (this.hardButton.isSelected()) {
+                    this.leaderboardText = handleWordHardHighScore();
+                }
+            } else if (this.sumEliminationButton.isSelected()) {
+                if (this.easyButton.isSelected()) {
+                    this.leaderboardText = handleSumEasyHighScore();
+                } else if (this.mediumButton.isSelected()) {
+                    this.leaderboardText = handleSumMediumHighScore();
+                } else if (this.hardButton.isSelected()) {
+                    this.leaderboardText = handleSumHardHighScore();
+                }
+            }
+        } catch (Exception ee) {
+            ee.printStackTrace();
+        }
+    }
+
+    private void switchAndShowDesired() {
+        this.gettingInformation = !this.gettingInformation;
+        this.errorText = "";
+        repaint();
+    }
+
+    private boolean isAllNumbers(String text) {
+        return text.matches("[0-9]+");
+    }
+
+    // MODIFIES: this
+    // EFFECT: Prints out the top requested players for Word Recollection Hard
+    private String handleWordHardHighScore() throws InvocationTargetException, IllegalAccessException {
+        StringBuilder stringBuilder = new StringBuilder();
+        topN = leaderboards.getWordRecollectionHard().getTopNPlayers(numOfPlayersToView);
+        for (Player player : topN) {
+            stringBuilder.append(position).append(". ").append(player.getName())
+                    .append(" has a score of ").append(player.getWordRecollectionHardHighScore()).append("\n");
+            position++;
+        }
+        return stringBuilder.toString();
+    }
+
+    // MODIFIES: this
+    // EFFECT: Prints out the top requested players for Word Recollection Medium
+    private String handleWordMediumHighScore() throws InvocationTargetException, IllegalAccessException {
+        StringBuilder stringBuilder = new StringBuilder();
+        topN = leaderboards.getWordRecollectionMedium().getTopNPlayers(numOfPlayersToView);
+        for (Player player : topN) {
+            stringBuilder.append(position).append(". ").append(player.getName())
+                    .append(" has a score of ").append(player.getWordRecollectionMediumHighScore()).append("\n");
+            position++;
+        }
+        return stringBuilder.toString();
+    }
+
+    // MODIFIES: this
+    // EFFECT: returns the top requested players for Word Recollection Easy
+    private String handleWordEasyHighScore() throws InvocationTargetException, IllegalAccessException {
+        StringBuilder stringBuilder = new StringBuilder();
+        topN = leaderboards.getWordRecollectionEasy().getTopNPlayers(numOfPlayersToView);
+        for (Player player : topN) {
+            stringBuilder.append(position).append(". ").append(player.getName())
+                    .append(" has a score of ").append(player.getWordRecollectionEasyHighScore()).append("\n");
+            position++;
+        }
+        return stringBuilder.toString();
+    }
+
+    // MODIFIES: this
+    // EFFECT: returns the top requested players for Sum Elimination Hard
+    private String handleSumHardHighScore() throws InvocationTargetException, IllegalAccessException {
+        StringBuilder stringBuilder = new StringBuilder();
+        topN = leaderboards.getSumEliminationHard().getTopNPlayers(numOfPlayersToView);
+        for (Player player : topN) {
+            stringBuilder.append(position).append(". ").append(player.getName())
+                    .append(" has a score of ").append(player.getSumEliminationHardHighScore()).append("\n");
+            position++;
+        }
+        return stringBuilder.toString();
+    }
+
+    // MODIFIES: this
+    // EFFECT: returns the top requested players for Sum Elimination Medium
+    private String handleSumMediumHighScore() throws InvocationTargetException, IllegalAccessException {
+        StringBuilder stringBuilder = new StringBuilder();
+        topN = leaderboards.getSumEliminationMedium().getTopNPlayers(numOfPlayersToView);
+        for (Player player : topN) {
+            stringBuilder.append(position).append(". ").append(player.getName()).append(" has a score of ")
+                    .append(player.getSumEliminationMediumHighScore()).append("\n");
+            position++;
+        }
+        return stringBuilder.toString();
+    }
+
+    // MODIFIES: this
+    // EFFECT: Returns the top requested players for Sum Elimination Easy
+    private String handleSumEasyHighScore() throws InvocationTargetException, IllegalAccessException {
+        StringBuilder stringBuilder = new StringBuilder();
+        topN = leaderboards.getSumEliminationEasy().getTopNPlayers(numOfPlayersToView);
+        for (Player player : topN) {
+            stringBuilder.append(position).append(". ").append(player.getName()).append(" has a score of ")
+                    .append(player.getSumEliminationEasyHighScore()).append("\n");
+            position++;
+        }
+        return stringBuilder.toString();
+    }
+
 }
